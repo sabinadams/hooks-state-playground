93fc892b8d5fa3635eea1dc9c890b691cb8ecc0e
Sabin <sabintheworld@gmail.com>
2019-09-29 00:20:04 -0700

    Removes Shared State Stuff

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.scss'
 import PointsUseState from './components/PointsUseState'
 import PointsUseReducer from './components/PointsUseReducer'
-import StateApp from './StateApp'
 
 function App() {
   return (
     <div className="App">
       <PointsUseState />
       <PointsUseReducer />
-      <StateApp />
     </div>
   );
 }
